05d45e7283b3a2d87db5d85b83555359f48aece4
drdon1234 <96719161+drdon1234@users.noreply.github.com>
2025-04-05 02:28:23 +0800

    Update temp2.py

--- temp2.py
+++ temp2.py
@@ -1,8 +1,7 @@
 import yaml
 import aiohttp
-from pathlib import Path
 
-config_path = Path(__file__).parent.parent.parent / "config.yaml"
+config_path = "./config.yaml"
 
 with open(config_path, "r", encoding="utf-8") as file:
     config = yaml.safe_load(file)
